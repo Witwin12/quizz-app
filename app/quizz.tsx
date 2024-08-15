605ef1f2f0caaba4127db78a5068c7b71738d8c8
SNext <snext2004@gmail.com>
2024-08-15 16:33:32 +0700

    add: แสดงผล ที่เลือกไป

--- app/quizz.tsx
+++ app/quizz.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import QuizzCom from '@/components/QuizzCom';
 
@@ -22,6 +22,10 @@ export default function Quizz() {
     updatedAnswers[currentPage - 1] = answer;
     setSelectedAnswers(updatedAnswers);
   };
+
+  useEffect(() => {
+    console.log(selectedAnswers);
+  }, [selectedAnswers]);
 
   return (
     <View style={styles.container}>
